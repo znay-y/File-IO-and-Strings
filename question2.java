c7e5abbf2068488d41f40d008e3f0c4854f1f1a5
Z Laptop <zaineirshad.07@gmail.com>
2026-04-02 21:01:15 +0100

    Complete question 2

---
+++ question2.java
@@ -0,0 +1,10 @@
+public class question2 {
+public static void main(String[] args) {
+    
+}
+
+static int countWords(String line){
+    String [] words= line.split(" ");
+    return words.length;
+}
+}
