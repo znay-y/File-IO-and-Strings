c29b5ea32d78a808fcb2ad36f2cab25b0615e7da
Z PC <zaineirshad.07@gmail.com>
2026-04-03 08:53:35 +0100

    Completed Question 5

--- question5.java
+++ question5.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class question5 {
     public static void main(String[] args) {
@@ -21,13 +22,60 @@ public class question5 {
     }
 
     static boolean checkLine(String input) {
-    
+        String[] parts = input.split(",");
 
+        if (parts.length == 3) {
+            //System.out.println("Name found: " + parts[0]);
+            //System.out.println("E-mail found: " + parts[1]);
+            //System.out.println("Age found: " + parts[2]);
+
+            boolean isEmailValid = emailCheck(parts[1]);
+            boolean isAgeValid = AgeCheck(parts[2]);
+
+            if (isAgeValid==true&&isEmailValid==true){
+                return true;
+            }
+        } else {
+            //System.out.println("Input does NOT fit the format");
+        }
+        return false;
+
+    }
+
+    static boolean emailCheck(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        if (email.matches(regex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean AgeCheck(String text) {
+        boolean valid = false;
+        String regex = "^[0-9]";
+        String[] letters = text.split("");
+
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i].matches(regex)) {
+
+            } else {
+                return false;
+            }
+
+        }
         return true;
     }
 
     static String printLine(String input) {
-return null;
+        String[] parts = input.split(",");
+
+        String name = parts[0];
+        String email = parts[1];
+        String age = parts[2];
+
+
+        return (name + " ("+age+") - "+email);
     }
 
 }
